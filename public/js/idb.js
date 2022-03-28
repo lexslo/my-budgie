c3dd665c7b5d164bac7404b0b9249815c8b2fc61
Sparkle Party <lexi.slovik@gmail.com>
2022-03-28 15:19:05 -0700

    add idb.js and service worker

---
+++ public/js/idb.js
@@ -0,0 +1,87 @@
+// create variable to hold db connection
+let db;
+
+// establish a connection to IndexedDB database called 'my_budgie' and set it to version 1
+const request = indexedDB.open('my_budgie', 1);
+
+request.onupgradeneeded = function(event) {
+    // save a reference to the database 
+    const db = event.target.result;
+    // create an object store, set autoIncrement to true
+    db.createObjectStore('new_expense', { autoIncrement: true });
+};
+
+// upon a successful 
+request.onsuccess = function(event) {
+    // when db is successfully created with its object store (from onupgradedneeded event above) 
+    //or simply established a connection, save reference to db in global variable
+    db = event.target.result;
+
+    // check if app is online, if yes run uploadChanges() function to send all local db data to api
+    if (navigator.onLine) {
+        uploadChanges();
+    }
+};
+
+request.onerror = function(event) {
+    // log error here
+    console.log(event.target.errorCode);
+};
+
+// This function will be executed if we attempt to submit an expense and there's no internet connection
+function saveRecord(record) {
+    // open a new transaction with the database with read and write permissions 
+    const transaction = db.transaction(['new_expense'], 'readwrite');
+
+    // access the object store for `new_expense`
+    const expenseObjectStore = transaction.objectStore('new_expense');
+
+    // add record to your store with add method
+    expenseObjectStore.add(record);
+}
+
+function uploadChanges() {
+    // open a transaction on your db
+    const transaction = db.transaction(['new_expense'], 'readwrite');
+
+    // access your object store
+    const expenseObjectStore = transaction.objectStore('new_expense');
+
+    // get all records from store and set to a variable
+    const getAll = expenseObjectStore.getAll();
+
+    // upon a successful .getAll() execution, run this function
+    getAll.onsuccess = function() {
+        // if there was data in indexedDb's store, let's send it to the api server
+        if (getAll.result.length > 0) {
+            fetch('/api/transaction/bulk', {
+                    method: 'POST',
+                    body: JSON.stringify(getAll.result),
+                    headers: {
+                        Accept: 'application/json, text/plain, */*',
+                        'Content-Type': 'application/json'
+                    }
+                })
+                .then(response => response.json())
+                .then(serverResponse => {
+                    if (serverResponse.message) {
+                        throw new Error(serverResponse);
+                    }
+                    // open one more transaction
+                    const transaction = db.transaction(['new_expense'], 'readwrite');
+                    // access the new_pizza object store
+                    const expenseObjectStore = transaction.objectStore('new_expense');
+                    // clear all items in your store
+                    expenseObjectStore.clear();
+
+                    alert('All saved changes has been submitted to your budget!');
+                })
+                .catch(err => {
+                    console.log(err);
+                });
+        }
+    };
+}
+
+// listen for app coming back online
+window.addEventListener('online', uploadChanges);
